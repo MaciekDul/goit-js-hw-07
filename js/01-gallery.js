10acd88d07f94ff0b7d3391dcf3ad2b88d3b0cfe
MaciekDul <maciej.dul@onet.pl>
2023-02-26 20:06:06 +0100

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector("div.gallery");
+const imageGallery = galleryItems
+  .map(
+    (image) => `<div class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", imageGallery);
+
+gallery.addEventListener("click", selectImgages);
+
+function selectImgages(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    const selectedImgSource = event.target.dataset.source;
+    const instance = basicLightbox.create(
+      `<img src="${selectedImgSource}" width="1400" height="900">`,
+      {
+        onShow: () => {
+          window.addEventListener("keydown", escapeKey);
+        },
+        onClose: () => {
+          window.removeEventListener("keydown", escapeKey);
+        },
+      }
+    );
+
+    const escapeKey = (event) => {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    };
+
+    instance.show();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const SimpleLight = document.querySelector("ul.gallery");
+
+const SimpleLightGallery = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+  )
+  .join("");
+
+SimpleLight.insertAdjacentHTML("afterbegin", SimpleLightGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captiondelay: 250,
+});
